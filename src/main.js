497ccce17182606af6ed658723964aa56e37c8eb
Alexandre Six <alexandrenilton@gmail.com>
2018-12-19 19:25:29 -0200

    eslint and others

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const a = 3;
+const b = 2;
+const d = 4;
+
+console.log(a, b, d);
+const c = 'asdfasdfasdfasdfasdfasdfasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd';
+
+console.log(c);
